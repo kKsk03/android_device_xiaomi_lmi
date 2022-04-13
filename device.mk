5e80dba64cf879fcacc773628035415b9bebf676
kKsk <alonzomax1210@gmail.com>
2022-04-13 14:10:48 +0800

    lmi: remove excess

--- device.mk
+++ device.mk
@@ -435,9 +435,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service-qti
 
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/usb/etc
-
 # Vendor service manager
 PRODUCT_PACKAGES += \
     vndservicemanager
