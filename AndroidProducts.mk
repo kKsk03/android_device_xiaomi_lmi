70f97e39c2875fd0b1d58a00182536128a4b55a0
kKsk <alonzomax1210@gmail.com>
2022-12-15 14:18:49 +0800

    lmi: Update device tree
    Discard the old device tree and update the file to the device tree of the official branch of Pixel Experience

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_lmi.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_lmi-eng \
+    aosp_lmi-user \
+    aosp_lmi-userdebug
---
+++ aosp_lmi.mk
@@ -0,0 +1,41 @@
+#
+# Copyright (C) 2021 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
+# Inherit from lmi device
+$(call inherit-product, device/xiaomi/lmi/device.mk)
+
+# Inherit some common AOSP stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
+
+# Inherit some common Project Elixir stuff.
+ELIXIR_BUILD_TYPE := OFFICIAL
+ELIXIR_MAINTAINER := 家-|ZenkaBestia
+EXTRA_UDFPS_ANIMATIONS := true
+IS_PHONE := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_FACE_UNLOCK_SUPPORTED := true
+TARGET_SUPPORTS_GOOGLE_RECORDER := true
+TARGET_SUPPORTS_QUICK_TAP := true
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_lmi
+PRODUCT_DEVICE := lmi
+PRODUCT_BRAND := POCO
+PRODUCT_MODEL := POCO F2 Pro
+PRODUCT_MANUFACTURER := Xiaomi
+
+PRODUCT_CHARACTERISTICS := nosdcard
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+BUILD_FINGERPRINT := "Redmi/lmi/lmi:12/RKQ1.211001.001/V13.0.6.0.SJKCNXM:user/release-keys"
+
+PRODUCT_BUILD_PROP_OVERRIDES += \
+    PRIVATE_BUILD_DESC="lmi-user 12 RKQ1.211001.001 V13.0.6.0.SJKCNXM release-keys"
